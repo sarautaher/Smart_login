cfffc732f9c399f044725dfb74a49e1a863590ed
sarautaher <128692610+sarautaher@users.noreply.github.com>
2024-07-31 20:03:52 +0300

    Update style.js
    saea

--- js/style.js
+++ js/style.js
@@ -1,55 +1,78 @@
-var emaillog=document.getElementById('email');
-var passwordlog=document.getElementById('password');
-var link=document.getElementById('welcome');
-var Nameup=document.getElementById('Name');
-var emailup=document.getElementById('email');
-var passwordup=document.getElementById('password');
-var btn=document.getElementById('btn2');
-var exist=document.getElementById('exist');
-var demo=document.getElementById('demo');
-var btn=document.getElementsByClassName('btn');
-var welcomeUserLog=document.getElementById('welcomeUserLog');
-
-
- function add2(){
-         
-       var email=emaillog.value;
-        var password=passwordlog.value;
-        logArr=JSON.parse(localStorage.getItem('user'))||[];
-        if(isEmpty()==true){
-            document.getElementById('exist').innerHTML='<span class="text-danger m-3"> Please Enter All Input </span>';
-        }
-        else if(logArr.length==0){
-            document.getElementById('exist').innerHTML="incorrect email or password";
-        }
-        else{
-        for(var i=0; i<logArr.length;i++){
-           
-            if(logArr[i].password.toLowerCase()==password.toLowerCase() &&logArr[i].email.toLowerCase()==email.toLowerCase()){
-                localStorage.setItem('userNames', logArr[i].Name);
-               document.getElementById('exist').innerHTML="Success";
-               window.open('./page/welcome.html');
-             
-                return true;
-            }
-            else{
-                document.getElementById('exist').innerHTML="incorrect email or password";
-            }
-          
-        }
+let email=document.getElementById('email');
+let Name=document.getElementById('name');
+let password=document.getElementById('password');
+let rePassword=document.getElementById('rePassword');
+let phone=document.getElementById('phone');
+let exist=document.getElementById('exist');
+let btn=document.getElementById('btn');
+async function add(){
+let emaillog=email.value.trim() ;
+let namelog=Name.value.trim();
+let passwordlog=password.value.trim();
+let rePasswordlog=rePassword.value.trim();
+let phonelog=phone.value.trim()
+let data = {
+    email:emaillog,
+    password:passwordlog,
+    name:namelog,
+    rePassword:rePasswordlog,
+    phone:phonelog
+};
+function isEmpty(){
+    return emaillog.length===0 || namelog.length===0|| passwordlog.length===0||rePasswordlog.length===0||phonelog.length===0
+}
+function Validators(){
+   const namePattern=/^[A-Z][a-z]{3,8}$/
+   const emailPattern = /^[a-zA-Z0-9]+@gmail\.com$/;
+  const passwordPattern = /^[A-Za-z0-9]{6,}$/;
+   const phonePattern=/^01[0125][0-9]{8}$/
+    if(!namePattern.test(Name.value)){
+        exist.innerHTML="Name min Length 3 chars and  max Length 8 chars";
+        return false;
     }
-    
- }
-
-
-
- function isEmpty(){
-    if(passwordlog.value==''||emaillog.value==""){
+    if(!phonePattern.test(phone.value)){
+        exist.innerHTML="Invalid phone format";
+        return false;
+    }
+    if(!emailPattern.test(email.value)){
+        exist.innerHTML="Invalid email format";
+        return false;
+    }
+    if(!passwordPattern.test(password.value)){
+        exist.innerHTML="Password must contain at least 6 characters"
+        return false ;
+    }
+    if(passwordlog !== rePasswordlog){
+        exist.innerHTML="Re-entered repassword does not match the password"
+        return false ;
+    }
+    else{
         return true;
-      
-    }};
-
-
-/*btn.addEventListener('click',function(){
-    add2();
-})*/
+     }
+}
+if(isEmpty()){
+       exist.innerHTML='Please Enter All Input'
+    return;
+}
+if(!Validators()){
+    return;
+}
+const requestOptions = {
+    method: "POST", 
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data),
+  };
+  let response = await fetch('https://ecommerce.routemisr.com/api/v1/auth/signup', requestOptions);
+  let result = await response.json();
+  console.log(result.message)
+  if (result.message =='success') {
+    exist.innerHTML = result.message;
+} else {
+    exist.innerHTML = result.message
+} 
+}
+btn.addEventListener('click', function(){
+    add()
+})
